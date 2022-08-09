8ab09f30e61d917596923ddd3dbcda2599dad6c4
Viktor Norgren <v.norgren01@gmail.com>
2022-08-09 13:05:24 +0200

    Squashed commits

--- plugin/mutate/source/dextool/plugin/mutate/backend/database/standalone.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/database/standalone.d
@@ -439,6 +439,7 @@ struct DbDependency {
     /// The root must already exist or the whole operation will fail with an sql error.
     void set(const Path path, const DepFile[] deps) @trusted {
         static immutable insertDepSql = "INSERT OR IGNORE INTO " ~ depFileTable ~ " (file,checksum)
+            ~ " (file,checksum)
             VALUES(:file,:cs)
             ON CONFLICT (file) DO UPDATE SET checksum=:cs WHERE file=:file";
 
@@ -1548,7 +1549,7 @@ struct DbMutant {
     }
 
     Nullable!MutationStatusId getMutationStatusId(const Checksum cs) @trusted {
-        static immutable sql = format!"SELECT id FROM %s WHERE checksum=:cs"(mutationStatusTable);
+        static immutable sql = format!"SELECT id FROM %s WHERE checksum=:cs"(
         auto stmt = db.prepare(sql);
         stmt.get.bind(":cs", cast(long) cs.c0);
 
@@ -1879,7 +1880,7 @@ struct DbMutant {
     ], true);
 
     Nullable!Checksum getChecksum(MutationStatusId id) @trusted {
-        static immutable sql = format!"SELECT checksum FROM %s WHERE id=:id"(mutationStatusTable);
+        static immutable sql = format!"SELECT checksum FROM %s WHERE id=:id"(
         auto stmt = db.prepare(sql);
         stmt.get.bind(":id", id.get);
 
@@ -2965,8 +2966,7 @@ private:
 MarkedMutant make(MarkedMutantTbl m) {
     import dextool.plugin.mutate.backend.type;
 
-    return MarkedMutant(m.mutationStatusId.MutationStatusId, Checksum(m.checksum),
-            m.mutationId.MutationId, SourceLoc(m.line, m.column), m.path.Path,
+    return MarkedMutant(m.mutationStatusId.MutationStatusId, Checksum(m.checksum), m.mutationId.MutationId, SourceLoc(m.line, m.column),
             m.toStatus.to!(Mutation.Status), m.time, m.rationale.Rationale, m.mutText);
 }
 
